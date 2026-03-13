7c65f76ae3f38d3209b3cd1164acf975173d5abf
Kasjan Michalak <kasjan.michalak@gmail.com>
2026-03-13 17:45:31 +0000

    2nd save

--- app/main.py
+++ app/main.py
@@ -12,18 +12,14 @@ def calculate_profit(trades_json: str) -> None:
     matecoin_account = Decimal("0")
 
     for trade in trades:
-        if trade["bought"] is None:
-            trade["bought"] = 0
-        if trade["sold"] is None:
-            trade["sold"] = 0
-        matecoin_account += Decimal(trade["bought"]) - Decimal(trade["sold"])
-        earned_money += ((Decimal(trade["bought"]) - Decimal(trade["sold"]))
-                         * Decimal(trade["matecoin_price"]))
+        bought = Decimal(trade["bought"] or 0)
+        sold = Decimal(trade["sold"] or 0)
+        matecoin_account += bought - sold
+        earned_money += (sold - bought) * Decimal(trade["matecoin_price"])
 
     matecoin_account = str(matecoin_account)
     earned_money = str(earned_money)
-    result = {"earned_money": earned_money,
-              "matecoin_account": matecoin_account}
+    result = {"earned_money": earned_money, "matecoin_account": matecoin_account}
 
     with open("profit.json", "w") as f:
-        json.dump(result, f)
+        json.dump(result, f, indent=2)
